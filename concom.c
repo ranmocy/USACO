700c4468188c1c0e667947194655f45b85e18b27
Ranmocy Sheng <ranmocy@gmail.com>
2013-12-09 16:34:32 -0800

    2.3 concom, shorter version

--- concom.c
+++ concom.c
@@ -5,30 +5,14 @@
 */
 
 #include <stdio.h>
-typedef enum { false, true } bool;
 #define N 100
+typedef enum { false, true } bool;
 
-int E;                  /* edges in graph */
-int shares[N+1][N+1];   /* totally have */
-bool controls[N+1][N+1]; /* whether can controls */
-
-
-/* search how many shares of j which i can controls */
-void control(int i, int j)
-{
-    if (controls[i][j]) return;
-    controls[i][j] = true;
-
-    int k;
-    for (k = 1; k <= N; k++){
-        /* add shares of j to i */
-        shares[i][k] += shares[j][k];
-        /* control k if i chould */
-        if (shares[i][k] > 50) control(i, k);
-    }
-
-    return;
-}
+bool changed = true;
+short E,i,j,k;
+short p[N+1][N+1];       /* directly have */
+short shares[N+1][N+1];  /* totally have */
+bool controls[N+1][N+1]; /* whether can control */
 
 
 int main()
@@ -36,31 +20,30 @@ int main()
     FILE *fin = fopen("concom.in", "r");
     FILE *fout = fopen("concom.out", "w");
 
-    int i,j,k,t,l;
-    for (i = 1; i <= N; i++) controls[i][i] = true;
-
-    fscanf(fin, "%d", &E);
-    for (l = 1; l <= E; l++){
-        fscanf(fin, "%d %d %d", &i, &j, &t);
-        /* add shares p[i][j] to k if k controlss i */
-        for (k = 1; k <= N; k++){
-            if (controls[k][i]) shares[k][j] += t;
-        }
-        /* update controls */
-        for (k = 1; k <= N; k++){
-            if (shares[k][j] > 50) control(k, j);
-        }
+    fscanf(fin, "%hd", &E);
+    while (E--){
+        fscanf(fin, "%hd %hd %hd", &i, &j, &k);
+        p[i][j] = shares[i][j] = k;
     }
 
-    for (i = 1; i <= N; i++){
-        for (j = 1; j <= N; j++){
-            if ((i != j) && (controls[i][j])){
-                fprintf(fout, "%d %d\n", i, j);
-            }
-        }
+    while (changed){
+        changed = false;
+        for (i = 1; i <= N; i++)
+            for (j = 1; j <= N; j++)
+                if (shares[i][j] >= 50 && !controls[i][j]){
+                    controls[i][j] = true;
+                    for (k = 1; k <= N; k++)
+                        if (p[j][k]) {
+                            shares[i][k] += p[j][k];
+                            changed = true;
+                        }
+                }
     }
 
-    fclose(fin);
-    fclose(fout);
+    for (i = 1; i <= N; i++)
+        for (j = 1; j <= N; j++)
+            if (i != j && controls[i][j])
+                fprintf(fout, "%hd %hd\n", i, j);
+
     return 0;
 }
